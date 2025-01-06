a5a993d8925ea8e088b334054a9ee2d2a00ff5b7
David Mohl <git@d.sh>
2025-01-06 10:01:11 +0700

    Add wework-cli formula

---
+++ Formula/wework-cli.rb
@@ -0,0 +1,20 @@
+class WeeworkCli < Formula
+  include Language::Python::Virtualenv
+
+  desc "CLI tool for WeWork"
+  homepage "https://github.com/dvcrn/wework-cli"
+  url "https://github.com/dvcrn/wework-cli.git", branch: "main"
+  version "0.1.0"
+  license "MIT"
+
+  depends_on "python@3.11"
+  depends_on "pipx"
+
+  def install
+    system "pipx", "install", "git+https://github.com/dvcrn/wework-cli.git"
+  end
+
+  test do
+    system "wework", "--help"
+  end
+end
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Homebrew Tap
 
 This is a custom Homebrew tap containing my personal formulas.
+
+## Usage
+
+You can add this tap to your Homebrew installation using:
+
+```bash
+brew tap dvcrn/formulas
+```
+
+## Available Formulas
+
+- `wework-cli`: CLI tool for WeWork. Install with `brew install dvcrn/formulas/wework-cli`
