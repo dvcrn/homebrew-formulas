a52d43ef69385b3928df795e1d5902f079a3b716
David Mohl <git@d.sh>
2025-01-15 21:52:46 +0900

    Add bottle information

--- Formula/chainenv.rb
+++ Formula/chainenv.rb
@@ -7,6 +7,13 @@ class Chainenv < Formula
 
   head "https://github.com/dvcrn/chainenv.git"
 
+  bottle do
+    root_url "https://github.com/dvcrn/chainenv/releases/download/1.0.0"
+
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "45c55fc84c857f263911fc0fed470d03ded5a406e9fd4f66093b9b84929ed279"
+  end
+
   depends_on "go" => :build
 
   def install
