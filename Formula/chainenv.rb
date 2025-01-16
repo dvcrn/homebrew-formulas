3ec9f950d98d2f9b369d3e14c606c87bb25524ed
David Mohl <git@d.sh>
2025-01-16 11:07:54 +0900

    Bump version

--- Formula/chainenv.rb
+++ Formula/chainenv.rb
@@ -2,17 +2,12 @@ class Chainenv < Formula
   desc "CLI for storing and retrieving secrets"
 
   homepage "https://github.com/dvcrn/chainenv"
-  url "https://github.com/dvcrn/chainenv.git", tag: "1.0.0"
-  version "1.0.0"
+  VERSION = "1.1.0"
+
+  url "https://github.com/dvcrn/chainenv.git", tag: VERSION
+  version VERSION
 
   head "https://github.com/dvcrn/chainenv.git"
-
-  bottle do
-    root_url "https://github.com/dvcrn/chainenv/releases/download/1.0.0"
-
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "45c55fc84c857f263911fc0fed470d03ded5a406e9fd4f66093b9b84929ed279"
-  end
 
   depends_on "go" => :build
 
