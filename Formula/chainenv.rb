c91fa2bed6e31470ccfd0231900e526a760f8058
David Mohl <git@d.sh>
2025-01-16 11:28:16 +0900

    Rewrite chainenv Formula

--- Formula/chainenv.rb
+++ Formula/chainenv.rb
@@ -1,18 +1,20 @@
 class Chainenv < Formula
-  desc "CLI for storing and retrieving secrets"
+  desc "CLI for storing and retrieving secrets from Keychain and 1Password"
 
   homepage "https://github.com/dvcrn/chainenv"
-  VERSION = "1.1.0"
+  head "https://github.com/dvcrn/chainenv.git", branch: "main"
+  license "GPLv3"
 
-  url "https://github.com/dvcrn/chainenv.git", tag: VERSION
-  version VERSION
-
-  head "https://github.com/dvcrn/chainenv.git"
+  version "1.1.0"
+  url "https://github.com/dvcrn/chainenv/archive/refs/tags/1.1.0.tar.gz"
+  sha256 "b0727c31696fd90401d653fc96e1a66f473ce244d5dd6b3b59a471e9ce2b5be8"
 
   depends_on "go" => :build
 
   def install
     system "go", "build", "-ldflags", "-s -w", "-trimpath", "-o", bin/"chainenv", "main.go"
+
+    generate_completions_from_executable(bin/"chainenv", "completion")
   end
 
   test do
